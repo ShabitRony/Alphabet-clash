7cd70aac18819e8154000021028fdc45ea3ed272
ShabitRony <shabitalahi123@gmail.com>
2025-01-19 04:57:51 +0600

    play game part updated

--- JS/alpha-clash.js
+++ JS/alpha-clash.js
@@ -11,6 +11,7 @@
 function play(){
     hideElementById('home');
     showElementById('playground-section');
+    hideElementById('final-score')
     continueGame();
 }
 
@@ -23,3 +24,31 @@ function continueGame(){
 
     setBackgroundColorById(alphabet)
 }
+
+function handlePressKey(event){
+    const eventKey= event.key;
+
+   const show = document.getElementById('show-alphabet')
+   const showElement = show.innerText.toLowerCase();
+   console.log(showElement);
+   
+   
+ if(eventKey == showElement){
+    console.log("You got the alphabet");
+   
+    removeBackgroundColorById(showElement);
+    continueGame();
+    const currentScore = getTextElementValue('score')
+    const newScore = currentScore+1;
+    
+    
+ }else{
+    console.log("You lost a life");
+    minusLife();
+    
+ }
+
+    
+}
+document.addEventListener('keyup',handlePressKey)
+
--- JS/utility.js
+++ JS/utility.js
@@ -7,6 +7,7 @@ function showElementById(element){
     show.classList.remove('hidden');
 }
 
+
 function getARandomAlphabet(){
     const alphabetString ='abcdefghijklmnopqrstuvwxyz';
     const alphabets = alphabetString.split(['']);
@@ -20,4 +21,45 @@ function getARandomAlphabet(){
     function setBackgroundColorById(element){
         const elementById = document.getElementById(element);
         elementById.classList.add('bg-orange-400');
+    }
+
+    function removeBackgroundColorById(element){
+        const elementById = document.getElementById(element);
+        elementById.classList.remove('bg-orange-400');
+    }
+
+    function getTextElementValue(element){
+            const element =document.getElementById(element);
+            const elementText = element.innerText;
+            return parseFloat(elementText)
+    }
+
+    // function updateScore(){
+    // //     const currentScore = document.getElementById('score');
+    // //     const scoreText = currentScore.innerText;
+    // //     const score = parseFloat(scoreText);
+    // //     console.log(score);
+
+    // //     const newScore = score +1;
+
+    // //    currentScore.innerText = newScore;
+
+    // const score = document.getElementById('score');
+    // const scoreText = score.innerText;
+    // const newScore = parseFloat(scoreText)+1;
+    // score.innerText = newScore;   
+    // }
+    // function minusLife(){
+    //     const minusScore = document.getElementById('life');
+    //     const scoreText = minusScore.innerText;
+    //     const newScore = parseFloat(scoreText)-1;
+    //     minusScore.innerText = newScore;
+    //     if(newScore === 0){
+    //         gameOver();
+    //     }
+    // }
+
+    function gameOver(){
+        hideElementById('playground-section');
+        showElementById('final-score')
     }
